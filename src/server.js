afd0c2195b338f0949d19838d6009ad116ad2996
Your Name <2429013@kiit.ac.in>
2026-06-23 17:18:19 +0530

    fix: uncomment sequelize.sync() to ensure tables are synced successfully

--- src/server.js
+++ src/server.js
@@ -70,7 +70,7 @@ const startServer = async () => {
   try {
     await sequelize.authenticate();
     console.log("Database connected successfully");
-    // await sequelize.sync({ alter: true });
+   // await sequelize.sync({ alter: true });
     await sequelize.sync();
     // { alter: true }
     console.log("Tables synced successfully");
